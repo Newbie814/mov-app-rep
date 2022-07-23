fe54328dc14e38b23b344f2f1e669eb78e47e840
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-07-23 16:10:35 -0400

    set up api env variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const API_KEY = process.env.MOVIE_API_KEY;
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
